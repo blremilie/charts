fabc4ee661aff08759cfb80430517209c07fefe1
blremilie <bletteryemilie@gmail.com>
2024-04-26 14:33:28 +0200

    ajout responsive

--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
-import React, { useRef} from "react";
+import React from "react";
 import Chart from "chart.js/auto";
-import { Bar, getElementAtEvent } from "react-chartjs-2";
+import { Bar } from "react-chartjs-2";
 import "./App.css";
+
+const jsonData= require("./data.json");
+let labels = jsonData.map(function(e) { return e.day });
+let price = jsonData.map(function(e) { return e.amount });
+
+const HOVER_INDEX = 1;
+const BACKGROUND_INDEX = 0;
 
 Chart.register();
 
+function currentDayBackground() {
+  let backgroundTable = Array(7).fill("hsl(10, 79%, 65%)");
+  let hoverBackgroundTable = Array(7).fill("rgba(255,155,135,255)");
+  const date = new Date();
+  date.setDate(date.getDate() - 1);
+  const day = date.getDay(); 
+
+  backgroundTable[day] = "hsl(186, 34%, 60%)";
+  hoverBackgroundTable[day] = "rgba(180,223,229,255)";
+
+  return [backgroundTable, hoverBackgroundTable];
+}
+
+let backgroundTables = currentDayBackground();
+
 export const data = {
-  labels: ["mon", "thu", "wed", "thu", "fri", "sat", "sun"],
+  labels: labels,
   datasets: [
     {
-      data: [15, 35, 52.36, 31.07, 25, 48, 28],
-      backgroundColor: ["hsl(10, 79%, 65%)",
-                        "hsl(10, 79%, 65%)",
-                        "hsl(186, 34%, 60%)",
-                        "hsl(10, 79%, 65%)",
-                        "hsl(10, 79%, 65%)",
-                        "hsl(10, 79%, 65%)",
-                        "hsl(10, 79%, 65%)"],
-      hoverBackgroundColor: ["rgba(255,155,135,255)",
-                            "rgba(255,155,135,255)", 
-                            "rgba(180,223,229,255)",
-                            "rgba(255,155,135,255)", 
-                            "rgba(255,155,135,255)", 
-                            "rgba(255,155,135,255)", 
-                            "rgba(255,155,135,255)" ],
+      data: price,
+      backgroundColor: backgroundTables[BACKGROUND_INDEX],
+      hoverBackgroundColor: backgroundTables[HOVER_INDEX],
       borderRadius: 5,
       borderSkipped: false,
     },
   ],
 };
+
 
 export const options = {
   scales: {
@@ -58,27 +69,31 @@ export const options = {
       display: false,
     },
     tooltip: {
-      enabled: true,
-      backgroundColor: "hsl(25, 47%, 15%)",
-      afterTitle: "string[]",
+      tooltipCaretSize: 0,
+      callbacks: {
+        label: function(context) {
+            let label = context.dataset.label || '';
+            if (label) {
+                label += ': ';
+            }
+            if (context.parsed.y !== null) {
+                label += new Intl.NumberFormat('en-US', 
+                { style: 'currency', currency: 'USD' }).format(context.parsed.y);
+            }
+            return label;
+        },
+      }
     },
-  responsive: true,
+    responsive: true,
   }
 }
 
-export default function App() {
-  const chartRef = useRef();
-  const onHover = (event) => {
-    const elements = getElementAtEvent(chartRef.current, event);
-    console.log(elements) 
-  }
+export default function App (){
 
 return (
   <Bar
-    ref= {chartRef}
     options={options}
     data={data}
-    onMouse={onHover}
   />
 );
 }
